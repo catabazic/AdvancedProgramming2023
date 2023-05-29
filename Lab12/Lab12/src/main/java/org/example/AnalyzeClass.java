3fd6fb338755ce6fc1922406258d55e662f021e2
Bazic Catalina <bazic.catalina@gmail.com>
2023-05-29 22:09:40 +0300

    :aerial_tramway:

---
+++ Lab12/Lab12/src/main/java/org/example/AnalyzeClass.java
@@ -0,0 +1,126 @@
+package org.example;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+
+public class AnalyzeClass {
+
+    public static void analyze(String path) {
+        File file = new File(path);
+
+        if (file.isDirectory()) {
+            // Input is a folder, explore it recursively
+            exploreFolder(file);
+        } else if (file.isFile() && file.getName().endsWith(".jar")) {
+            // Input is a .jar file
+            analyzeJar(file);
+        } else if (file.isFile() && file.getName().endsWith(".class")) {
+            analyzeClassFile(file);
+        }else {
+            throw new IllegalArgumentException("Invalid input: " + path);
+        }
+    }
+
+    private static void exploreFolder(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    exploreFolder(file);
+                } else if (file.isFile() && file.getName().endsWith(".class")) {
+                    analyzeClassFile(file);
+                }
+            }
+        }
+    }
+
+    private static void analyzeJar(File jarFile) {
+        try {
+            URL jarURL = jarFile.toURI().toURL();
+            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{jarURL});
+
+            // Iterate over the entries in the jar file
+            // and analyze only the .class files
+            // assuming that the classes are in the root directory of the jar
+            JarFile jar = new JarFile(jarFile);
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                    String className = entry.getName().replace("/", ".").substring(0, entry.getName().length() - 6);
+                    analyzeClassName(jarFile.getAbsolutePath());
+                }
+            }
+            jar.close();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Failed to load jar file: " + jarFile, e);
+        } catch (java.io.IOException e) {
+            throw new RuntimeException("Error while exploring jar file: " + jarFile, e);
+        }
+    }
+
+    private static void analyzeClassFile(File classFile) {
+            analyzeClassName(classFile.getAbsolutePath());
+    }
+    public static void analyzeClassName(String urlClass) {
+        try {
+            // Load the specified class in memory
+            File file = new File(urlClass);
+            MyClassLoader myClassLoader = new MyClassLoader();
+            if (file.exists()) {
+                URL url = file.toURI().toURL();
+                myClassLoader.addURL(url);
+            }
+            String className = getClassName(urlClass);
+
+            Class myClass = myClassLoader.loadClass(urlClass);
+
+            System.out.println(myClass.getName());
+            System.out.println(myClass.getPackage());
+            System.out.println(Arrays.toString(myClass.getMethods()));
+            System.out.println(myClass.getSuperclass());
+
+            int passed = 0, failed = 0;
+            for (Method method : Class.forName(myClass.getName()).getMethods()) {
+                if (method.isAnnotationPresent(Test.class)) {
+                    try {
+                        method.invoke(null);
+                        passed++;
+                    } catch (Throwable ex) {
+                        System.out.printf("Test %s failed: %s %n", method, ex.getCause());
+                        failed++;
+                    }
+                }
+            }
+            System.out.printf("Passed: %d, Failed %d%n", passed, failed);
+        } catch (ClassNotFoundException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getClassName(String classFilePath) {
+        String separator = File.separator;
+        if (separator.equals("\\")) {
+            // Windows separator is backslash, replace forward slashes with backslashes
+            classFilePath = classFilePath.replace('/', '\\');
+        }
+
+        int packageStartIndex = classFilePath.indexOf("classes" + separator) + 8;
+        int packageEndIndex = classFilePath.lastIndexOf(separator);
+        String packagePath = classFilePath.substring(packageStartIndex, packageEndIndex).replace(separator, ".");
+
+        String className = classFilePath.substring(packageEndIndex + 1, classFilePath.lastIndexOf(".class"));
+        return packagePath + "." + className;
+    }
+
+}
--- Lab12/Lab12/src/main/java/org/example/Main.java
+++ Lab12/Lab12/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        AnalyzeClass.analyze(
+                "D:\\IT\\FII 2sem2\\PA\\AdvancedProgramming2023\\Lab12\\Lab12\\target\\classes\\org\\example\\Program.class");
+
+
     }
 }
